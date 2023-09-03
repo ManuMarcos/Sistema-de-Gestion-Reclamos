693223f256d37bab3a300d8a7a281169eaeffdc1
eliaslamanna <eliaslamanna@hotmail.com>
2023-09-03 16:15:10 -0300

    usuario controller y service

---
+++ TPO_Aplicaciones_Interactivas_G04/src/main/java/api/tpo_g04_reclamos/app/controller/UsuarioController.java
@@ -0,0 +1,15 @@
+package api.tpo_g04_reclamos.app.controller;
+
+import api.tpo_g04_reclamos.app.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+}
