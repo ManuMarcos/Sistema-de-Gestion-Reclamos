7ef1623120ac8ca2c3c35c90e4be0bf39026b9a6
alaniv <ivoalani@outlook.com>
2023-09-03 15:22:40 -0300

    Usuario Entity

--- TPO_Aplicaciones_Interactivas_G04/src/main/java/api/tpo_g04_reclamos/app/model/entity/Usuario.java
+++ TPO_Aplicaciones_Interactivas_G04/src/main/java/api/tpo_g04_reclamos/app/model/entity/Usuario.java
@@ -1,15 +1,45 @@
 package api.tpo_g04_reclamos.app.model.entity;
 
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String nombre;
 	private String password;
+	
+	@Column(name = "fecha_creacion")
+	@Temporal(TemporalType.DATE)
+	private Date fechaCreacion;
+
 
 	public Usuario(String nombre, String password) {
 		super();
 		this.nombre = nombre;
 		this.password = password;
+		this.fechaCreacion = new Date();
 	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
 	public String getNombre() {
 		return nombre;
 	}
@@ -24,5 +54,19 @@ public class Usuario {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Id:[%d];Nombre:[%s];Password[%s];FechaCreacion:[%s]", 
+				this.id, this.nombre, this.password, this.fechaCreacion.toString());
 	}
 }
