2b693d95a28976eadfccae2c11581f3a9630e467
eliaslamanna <eliaslamanna@hotmail.com>
2023-09-06 23:32:57 -0300

    reclamo entity

--- TPO_Aplicaciones_Interactivas_G04/src/main/java/api/tpo_g04_reclamos/app/model/entity/Reclamo.java
+++ TPO_Aplicaciones_Interactivas_G04/src/main/java/api/tpo_g04_reclamos/app/model/entity/Reclamo.java
@@ -1,5 +1,33 @@
 package api.tpo_g04_reclamos.app.model.entity;
 
-public abstract class Reclamo {
+import api.tpo_g04_reclamos.app.model.enums.EstadoReclamo;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reclamo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int numero;
+
+    private List<Imagen> imagenes = new ArrayList<>();
+
+    private String descripcion;
+
+    // edificio deberia borrarse del UML, referencia la tiene unidad
+
+    private EstadoReclamo estado;
+
+    private Usuario usuario;
+
+    private Unidad unidad;
+
+    private AreaComun areaComun;
 
 }
