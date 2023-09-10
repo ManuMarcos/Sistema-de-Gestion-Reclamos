eb845ac901c18719b03410616839fb89d89007b9
alaniv <ivoalani@outlook.com>
2023-09-10 13:07:35 -0300

    Image entity

--- TPO_Aplicaciones_Interactivas_G04/src/main/java/api/tpo_g04_reclamos/app/model/entity/Imagen.java
+++ TPO_Aplicaciones_Interactivas_G04/src/main/java/api/tpo_g04_reclamos/app/model/entity/Imagen.java
@@ -1,5 +1,7 @@
 package api.tpo_g04_reclamos.app.model.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
 import jakarta.persistence.*;
 
 @Entity
@@ -7,7 +9,68 @@ import jakarta.persistence.*;
 public class Imagen {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    
+    private String nombre;
+    private String tipo;
+    @Lob
+    private byte[] data;
+    
+    
+	public Imagen() {
+		super();
+	}
 
+
+	public Imagen(String nombre, String tipo, byte[] data) {
+		super();
+		this.nombre = nombre;
+		this.tipo = tipo;
+		this.data = data;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getTipo() {
+		return tipo;
+	}
+
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+
+	public byte[] getData() {
+		return data;
+	}
+
+
+	public void setData(byte[] data) {
+		this.data = data;
+	}
+    
+	
+    
 }
