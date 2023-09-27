8ce348cd456b345f66fe5d492d578d0fe1eac2fa
ManuMarcos <manuelignaciomarcos@hotmail.com>
2023-09-27 12:40:16 -0300

    Agrego el AuthController para el Login y el Bean SecretKey en SecurityConfig para generar la key para firmar el token

--- TPO_Aplicaciones_Interactivas_G04/src/main/java/api/tpo_g04_reclamos/app/config/SecurityConfig.java
+++ TPO_Aplicaciones_Interactivas_G04/src/main/java/api/tpo_g04_reclamos/app/config/SecurityConfig.java
@@ -1,11 +1,18 @@
 package api.tpo_g04_reclamos.app.config;
 
+import java.util.Base64;
+
+import javax.crypto.SecretKey;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
+
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 @Configuration
 @EnableWebSecurity
@@ -24,10 +31,20 @@ public class SecurityConfig {
     WebSecurityCustomizer webSecurityCustomizer() {
     	
     	//Aquellas urls que esten aca no requeriran autenticacion
-    	return (web) -> web.ignoring().requestMatchers("auth/login");
+    	return (web) -> web.ignoring().requestMatchers("auth/login", "saludos/hola");
     }
 	
-	
+    @Bean
+	SecretKey secretKey() {
+		//Genera una clave por si solo para el algoritmo HS256
+		SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+		
+		byte[] encodedKey = secretKey.getEncoded();
+		String encodedKeyBase64 = Base64.getEncoder().encodeToString(encodedKey);
+		System.out.println("Secret Key (Base64): " + encodedKeyBase64);
+		
+		return secretKey;
+	}
 	
 	
 }
---
+++ TPO_Aplicaciones_Interactivas_G04/src/main/java/api/tpo_g04_reclamos/app/controller/AuthController.java
@@ -0,0 +1,51 @@
+package api.tpo_g04_reclamos.app.controller;
+
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import api.tpo_g04_reclamos.app.controller.dto.UsuarioDto;
+import api.tpo_g04_reclamos.app.service.IUsuarioService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+	private final int EXPIRATION_TIME_IN_MIN = 1;
+	
+	@Autowired
+	private IUsuarioService usuarioService;
+	
+	@Autowired
+	private SecretKey secretKey;
+	
+	@PostMapping("/login")
+	public ResponseEntity<String> login(@RequestBody UsuarioDto credentials){
+		if(usuarioService.findUser(credentials.getNombre(), credentials.getPassword()).isPresent()) {
+			String token = 
+					Jwts.builder()
+					.setSubject(credentials.getNombre())
+					.setIssuedAt(new Date())
+					.setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME_IN_MIN * 60 * 1000))
+					.signWith(secretKey, SignatureAlgorithm.ES256)
+					.compact();
+					
+			return new ResponseEntity<>(token, HttpStatus.OK);
+		}
+		else {
+			return new ResponseEntity<>("Credenciales invalidas", HttpStatus.UNAUTHORIZED);
+		}
+	}
+	
+	
+}
