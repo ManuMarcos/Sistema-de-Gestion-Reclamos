a1341063cd88d87c9d45a2bc7f77d52b6f6b9a85
alaniv <ivoalani@outlook.com>
2023-11-14 22:18:42 -0300

    fetch imagen por id. lo probe con una subida a manopla

--- Front/app/src/components/Reclamos/DetalleReclamo/detalleReclamo.js
+++ Front/app/src/components/Reclamos/DetalleReclamo/detalleReclamo.js
@@ -11,6 +11,24 @@ const ImagenFruta = {
     "reclamo": null
 }
 
+async function get_imagen(id_imagen) {
+  // TODO: el controller no tiene un filtro por id por el momento... lo filtro por front...
+  // Default options are marked with *
+  const response = await fetch("http://localhost:8080/imagen/" + id_imagen, {
+    method: "GET", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    headers: {
+      "Content-type": "application/json",
+      "Access-Control-Allow-Origin": "htpp://localhost:3000/",
+      "Access-Control-Allow-Methods": "POST, GET, PUT",
+      "Access-Control-Allow-Headers": "Content-Type",
+      Authorization: "Bearer " + sessionStorage.getItem("accessToken"),
+      "cache-control": "no-cache",
+    },
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
 const DetalleReclamo = () => {
   const location = useLocation();
   const { rec, edi } = location.state;
@@ -22,7 +40,12 @@ const DetalleReclamo = () => {
     if(rec["imagenesIds"] != null && rec["imagenesIds"].length > 0){
       for(let i=0; i < rec["imagenesIds"].length; i++ ){
         console.log(i)
-        //TODO: setImages((images) => [...images, e.target.files[0]])
+        get_imagen(rec["imagenesIds"][i])
+        .then((data) => {
+          const buff = Buffer.from(data["data"], 'base64');
+          let f = new File([buff], data["nombre"], {type:data["tipo"]});
+          setImages((images) => [...images, f])
+        }).catch((err) => {console.log(err)})
       }
     } else {
       // TODO: quitar luego, imagen fruta para probar
