04d467f5ad09e458750dfe8e37898df442fe4d6b
mateori0s <mateorios@live.com>
2023-11-19 19:42:46 -0300

    login test

--- Front/app/src/components/LogIn/login.js
+++ Front/app/src/components/LogIn/login.js
@@ -1,21 +1,68 @@
 import React from "react";
+import { useState } from "react";
 
 
 const Login = () => {
+    const [idEdificio, setIdEdificio] = useState(sessionStorage.getItem("edificioId"));
+
+    async function login_func(user, pass) {
+        // Default options are marked with *
+        const response = await fetch("http://localhost:8080/auth/login", {
+          method: "POST", // *GET, POST, PUT, DELETE, etc.
+          mode: "cors", // no-cors, *cors, same-origin
+          headers: {
+            "Content-type": "application/json",
+            'Access-Control-Allow-Origin': 'htpp://localhost:3000',
+            'Access-Control-Allow-Methods' : "POST, GET, PUT",
+            'Access-Control-Allow-Headers' : "Content-Type",
+            'cache-control' : 'no-cache'
+          },
+          body: JSON.stringify({
+            nombre : user,
+            password : pass
+          }) // body data type must match "Content-Type" header
+        });
+        return response.json(); // parses JSON response into native JavaScript objects
+      }
+
+      function SubmitLogin(e){
+        e.preventDefault()
+        console.log(e);
+        login_func(e.target[0].value,  e.target[1].value)
+        .then((data) => {
+            sessionStorage.setItem("accessToken", data["accessToken"]);
+            sessionStorage.setItem("userId", data["userId"]);
+            sessionStorage.setItem("userType", data["tipo"]);
+            sessionStorage.setItem("edificioId", data["edificioId"]);
+            setIdEdificio(sessionStorage.getItem("edificioId"))
+        })
+        .catch((err) => console.log(err))
+    }
+    //hay que ver donde ponerlo
+    function SubmitLogout(e)
+    {
+        e.preventDefault()
+        sessionStorage.removeItem("accessToken");
+        sessionStorage.removeItem("userId");
+        sessionStorage.removeItem("userType");
+        sessionStorage.removeItem("edificioId");
+        setIdEdificio(null);
+    }
+
     return (
         <div class ="d-flex justify-content-center">
             <div className="d-grid gap-2 col-6 ">
-                <form>
+                <form onSubmit={SubmitLogin}>
                     <div className="mb-3">
-                        <label for="exampleInputEmail1" className="form-label">Email</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        <label for="exampleInputEmail1" className="form-label">Usuario</label>
+                        <input type="text" className="form-control" id="user" aria-describedby="emailHelp" />
                     </div>
                     <div className="mb-3">
                         <label for="exampleInputPassword1" className="form-label">Contraseña</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" />
+                        <input type="text" className="form-control" id="pass" />
                     </div>
                     <div>
-                        <a href="/" className="btn btn-primary">Login</a>
+                    <input type="submit" className="btn btn-primary" value="Login"/>
                     </div>
                 </form>
                 <div className="mb-3">
--- Front/app/src/components/adminPermisos/adminPermisos.js
+++ Front/app/src/components/adminPermisos/adminPermisos.js
@@ -1,12 +1,113 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useSearchParams } from "react-router-dom";
 
+async function get_edificio(id_usuario) {
+  const response = await fetch(
+    "http://localhost:8080/usuarios" + id_usuario,
+    {
+      method: "GET", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      headers: {
+        "Content-type": "application/json",
+        "Access-Control-Allow-Origin": "htpp://localhost:3000/",
+        "Access-Control-Allow-Methods": "POST, GET, PUT",
+        "Access-Control-Allow-Headers": "Content-Type",
+        Authorization: "Bearer " + sessionStorage.getItem("accessToken"),
+        "cache-control": "no-cache",
+      },
+    }
+  );
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+async function get_reclamos(id_usuario) {
+  const response = await fetch(
+    "http://localhost:8080/usuarios?usuarioId=" + id_usuario,
+    {
+      method: "GET", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      headers: {
+        "Content-type": "application/json",
+        "Access-Control-Allow-Origin": "htpp://localhost:3000/",
+        "Access-Control-Allow-Methods": "POST, GET, PUT",
+        "Access-Control-Allow-Headers": "Content-Type",
+        Authorization: "Bearer " + sessionStorage.getItem("accessToken"),
+        "cache-control": "no-cache",
+      },
+    }
+  );
+  return response.json();
+}
 
 const AdminPermisos = () => {
-    return (
-        <div>
-            <h1> Administrador de permisos de usuarios </h1>
-        </div>
-    );
-}
+  const [datosEdificio, setDatosEdificio] = useState([]);
+  const [datosListadoReclamos, setDatosListadoReclamos] = useState([]);
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    let id_elegido = parseInt(searchParams.get("usuario_id"));
+    get_edificio(id_elegido)
+      .then((data) => {
+        setDatosEdificio(data);
+        console.log("GET OK");
+        get_reclamos(id_elegido)
+          .then((data) => {
+            console.log("GET II OK");
+            setDatosListadoReclamos(data);
+          })
+          .catch((err) => {
+            alert("No se pudo obtener reclamos del edificio");
+          });
+      })
+      .catch((err) => {
+        alert("No se pudo obtener datos del edificio");
+      });
+  }, [searchParams]);
+
+  return (
+    <div className="container">
+      <h1>Administrador de usuarios</h1>
+      <div className="form-group">
+      </div>
+      <hr />
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Usuario</th>
+            <th scope="col">Contrasena</th>
+            <th scope="col">Tipo de usuario</th>
+          </tr>
+        </thead>
+        <tbody>
+          {datosListadoReclamos ? (
+            datosListadoReclamos.map((usuario) => {
+              return (
+                <tr key={usuario["numero"]}>
+                  <th scope="row">{usuario["numero"]}</th>
+                  <td>{usuario["username"]}</td>
+                  <td>{usuario["pass"]}</td>
+                  <td>{usuario["tipouser"]}</td>
+                  <td>
+                    <Link
+                      to="/Reclamos/Detalle"
+                      state={{ rec: usuario, edi: datosEdificio }}
+                    >
+                      <button type="button" className="btn btn-primary">
+                        Ver detalles
+                      </button>
+                    </Link>
+                  </td>
+                </tr>
+              );
+            })
+          ) : (
+            <h2>Sin Datos...</h2>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default AdminPermisos;
