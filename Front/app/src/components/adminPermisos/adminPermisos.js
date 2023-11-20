4c9165c67b923a97e25ee8af36676e1a8e716181
mateori0s <mateorios@live.com>
2023-11-19 21:49:43 -0300

    ABM Usuarios (sin editar permisos)

--- Front/app/src/components/adminPermisos/adminPermisos.js
+++ Front/app/src/components/adminPermisos/adminPermisos.js
@@ -1,26 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useSearchParams } from "react-router-dom";
 
-async function get_edificio(id_usuario) {
-  const response = await fetch(
-    "http://localhost:8080/usuarios" + id_usuario,
-    {
-      method: "GET", // *GET, POST, PUT, DELETE, etc.
-      mode: "cors", // no-cors, *cors, same-origin
-      headers: {
-        "Content-type": "application/json",
-        "Access-Control-Allow-Origin": "htpp://localhost:3000/",
-        "Access-Control-Allow-Methods": "POST, GET, PUT",
-        "Access-Control-Allow-Headers": "Content-Type",
-        Authorization: "Bearer " + sessionStorage.getItem("accessToken"),
-        "cache-control": "no-cache",
-      },
-    }
-  );
-  return response.json(); // parses JSON response into native JavaScript objects
-}
-
-async function get_reclamos(id_usuario) {
+async function get_usuarios(id_usuario) {
   const response = await fetch(
     "http://localhost:8080/usuarios?usuarioId=" + id_usuario,
     {
@@ -40,20 +21,19 @@ async function get_reclamos(id_usuario) {
 }
 
 const AdminPermisos = () => {
-  const [datosEdificio, setDatosEdificio] = useState([]);
-  const [datosListadoReclamos, setDatosListadoReclamos] = useState([]);
+  const [datosAdminPermisos, setAdminPermisos] = useState([]);
   const [searchParams] = useSearchParams();
 
   useEffect(() => {
     let id_elegido = parseInt(searchParams.get("usuario_id"));
-    get_edificio(id_elegido)
+    get_usuarios(id_elegido)
       .then((data) => {
-        setDatosEdificio(data);
+        setAdminPermisos(data);
         console.log("GET OK");
-        get_reclamos(id_elegido)
+        get_usuarios(id_elegido)
           .then((data) => {
             console.log("GET II OK");
-            setDatosListadoReclamos(data);
+            setAdminPermisos(data);
           })
           .catch((err) => {
             alert("No se pudo obtener reclamos del edificio");
@@ -73,28 +53,30 @@ const AdminPermisos = () => {
       <table className="table">
         <thead>
           <tr>
-            <th scope="col">#</th>
+            <th scope="col">Id</th>
             <th scope="col">Usuario</th>
-            <th scope="col">Contrasena</th>
+            <th scope="col">Contraseña</th>
+            <th scope="col">Fecha de creación</th>
             <th scope="col">Tipo de usuario</th>
           </tr>
         </thead>
         <tbody>
-          {datosListadoReclamos ? (
-            datosListadoReclamos.map((usuario) => {
+          {datosAdminPermisos ? (
+            datosAdminPermisos.map((usuario) => {
               return (
-                <tr key={usuario["numero"]}>
-                  <th scope="row">{usuario["numero"]}</th>
-                  <td>{usuario["username"]}</td>
-                  <td>{usuario["pass"]}</td>
-                  <td>{usuario["tipouser"]}</td>
+                <tr key={usuario["id"]}>
+                  <th scope="row">{usuario["id"]}</th>
+                  <td>{usuario["nombre"]}</td>
+                  <td>{usuario["password"]}</td>
+                  <td>{usuario["fechaCreacion"]}</td>
+                  <td>{usuario["tipoUsuario"]}</td>
                   <td>
                     <Link
-                      to="/Reclamos/Detalle"
-                      state={{ rec: usuario, edi: datosEdificio }}
+                      to="/editar-permisos"
+                      state={{ rec: usuario, edi: datosAdminPermisos }}
                     >
                       <button type="button" className="btn btn-primary">
-                        Ver detalles
+                        Editar
                       </button>
                     </Link>
                   </td>
